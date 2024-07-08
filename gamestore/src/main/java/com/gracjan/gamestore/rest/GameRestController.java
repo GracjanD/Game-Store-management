c0083788f4e0e24a35aad99c2b3f82a9cb2ba9ad
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:30:06 +0200

    Add CRUD endpoints

--- gamestore/src/main/java/com/gracjan/gamestore/rest/GameRestController.java
+++ gamestore/src/main/java/com/gracjan/gamestore/rest/GameRestController.java
@@ -1,9 +1,11 @@
 package com.gracjan.gamestore.rest;
 
+import com.gracjan.gamestore.entity.Game;
 import com.gracjan.gamestore.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +16,36 @@ public class GameRestController {
     @Autowired
     public GameRestController(GameService gameService){
         this.gameService = gameService;
+    }
+
+    @GetMapping("/games")
+    public List<Game> getGames(){
+        return gameService.findAll();
+    }
+
+    @GetMapping("/games/{gameId}")
+    public Game getGameById(@PathVariable("gameId") long id) {
+        return gameService.findById(id);
+    }
+
+    @PostMapping("/games")
+    public Game createGame(@RequestBody Game game) {
+        game.setId(0);
+        return gameService.save(game);
+    }
+
+    @PutMapping("/games")
+    public Game updateGame(@RequestBody Game game) {
+        return gameService.save(game);
+    }
+
+    @DeleteMapping("/games")
+    public String deleteGames(){
+        return gameService.deleteAll();
+    }
+
+    @DeleteMapping("/games/{gameId}")
+    public String deleteGameById(@PathVariable("gameId") long id){
+        return gameService.deleteById(id);
     }
 }
