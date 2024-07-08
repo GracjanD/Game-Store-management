8a9693c8c90c0837196592fe0e90d342e79942fa
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:40:11 +0200

    Add global exception handlers

---
+++ gamestore/src/main/java/com/gracjan/gamestore/exception/GameExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.gracjan.gamestore.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GameExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<GameErrorResponse> exceptionHandler(GameNotFoundException exc){
+        GameErrorResponse error = new GameErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<GameErrorResponse> exceptionHandler(Exception exc){
+        GameErrorResponse error = new GameErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ gamestore/src/main/java/com/gracjan/gamestore/exception/GameNotFoundException.java
@@ -0,0 +1,16 @@
+package com.gracjan.gamestore.exception;
+
+public class GameNotFoundException extends RuntimeException{
+
+    public GameNotFoundException(String message) {
+        super(message);
+    }
+
+    public GameNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
