1b448b5464c75895f0f0f31147897f2519de97c8
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:18:26 +0200

    Add controller GameRestController

---
+++ gamestore/src/main/java/com/gracjan/gamestore/rest/GameRestController.java
@@ -0,0 +1,10 @@
+package com.gracjan.gamestore.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GameRestController {
+
+}
