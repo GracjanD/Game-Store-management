9147eef27fdd0d0b2b09433910be2adfd056b310
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 10:04:01 +0200

    Add unit parameterized test for findById

--- gamestore/src/test/java/com/gracjan/gamestore/GamestoreUnitTests.java
+++ gamestore/src/test/java/com/gracjan/gamestore/GamestoreUnitTests.java
@@ -8,12 +8,15 @@ import com.gracjan.gamestore.service.GameServiceImpl;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -50,6 +53,23 @@ public class GamestoreUnitTests {
 
         // verify if gameRepository was executed only once.
         verify(gameRepository, times(1)).findAll();
+    }
+
+    @ParameterizedTest(name="gameId={0}")
+    @ValueSource(longs = {0, 1})
+    @Order(2)
+    public void test_findById(long value){
+        // set up
+        when(gameRepository.findById(value)).thenReturn(Optional.ofNullable(games.get((int) value)));
+
+        // execute
+        Game game = gameService.findById(value);
+
+        // assert
+        assertNotNull(game, "Game shouldn't be null");
+
+        // verify
+        verify(gameRepository, times(1)).findById(value);
     }
 
     @BeforeEach
