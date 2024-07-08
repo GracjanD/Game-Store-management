6f962340f71dce4f35d1bfd8708a83458d08b7fa
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:28:40 +0200

    Add CRUD operations in Service

--- gamestore/src/main/java/com/gracjan/gamestore/service/GameService.java
+++ gamestore/src/main/java/com/gracjan/gamestore/service/GameService.java
@@ -1,4 +1,13 @@
 package com.gracjan.gamestore.service;
 
+import com.gracjan.gamestore.entity.Game;
+
+import java.util.List;
+
 public interface GameService {
+    List<Game> findAll();
+    Game findById(long id);
+    Game save(Game game);
+    String deleteById(long id);
+    String deleteAll();
 }
--- gamestore/src/main/java/com/gracjan/gamestore/service/GameServiceImpl.java
+++ gamestore/src/main/java/com/gracjan/gamestore/service/GameServiceImpl.java
@@ -1,8 +1,11 @@
 package com.gracjan.gamestore.service;
 
+import com.gracjan.gamestore.entity.Game;
 import com.gracjan.gamestore.repository.GameJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService{
@@ -13,4 +16,34 @@ public class GameServiceImpl implements GameService{
     public GameServiceImpl(GameJpaRepository gameRepository){
         this.gameRepository = gameRepository;
     }
+
+    @Override
+    public List<Game> findAll() {
+        return gameRepository.findAll();
+    }
+
+    @Override
+    public Game findById(long id) {
+        return gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Not found game with id: " + id));
+    }
+
+    @Override
+    public Game save(Game game) {
+        return gameRepository.save(game);
+    }
+
+    @Override
+    public String deleteById(long id) {
+        findById(id);
+        gameRepository.deleteById(id);
+        return "Deleted game with id: " + id;
+    }
+
+    @Override
+    public String deleteAll() {
+        long count = gameRepository.count();
+        gameRepository.deleteAll();
+        return "Deleted " + count + " games";
+    }
+
 }
