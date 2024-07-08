70b0a933e2ed23cb00f7987155397817b275ec84
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 10:16:53 +0200

    Add scan base packages

--- gamestore/src/main/java/com/gracjan/gamestore/GamestoreApplication.java
+++ gamestore/src/main/java/com/gracjan/gamestore/GamestoreApplication.java
@@ -3,7 +3,7 @@ package com.gracjan.gamestore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.gracjan.gamestore"})
 public class GamestoreApplication {
 
 	public static void main(String[] args) {
