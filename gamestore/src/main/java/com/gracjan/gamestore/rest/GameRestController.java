310481e6e9b2830e64de46e238917f9665348bd3
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:24:05 +0200

    Add constructor injections

--- gamestore/src/main/java/com/gracjan/gamestore/rest/GameRestController.java
+++ gamestore/src/main/java/com/gracjan/gamestore/rest/GameRestController.java
@@ -1,5 +1,7 @@
 package com.gracjan.gamestore.rest;
 
+import com.gracjan.gamestore.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class GameRestController {
 
+    private GameService gameService;
+
+    @Autowired
+    public GameRestController(GameService gameService){
+        this.gameService = gameService;
+    }
 }
--- gamestore/src/main/java/com/gracjan/gamestore/service/GameServiceImpl.java
+++ gamestore/src/main/java/com/gracjan/gamestore/service/GameServiceImpl.java
@@ -1,8 +1,16 @@
 package com.gracjan.gamestore.service;
 
+import com.gracjan.gamestore.repository.GameJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameServiceImpl implements GameService{
 
+    private GameJpaRepository gameRepository;
+
+    @Autowired
+    public GameServiceImpl(GameJpaRepository gameRepository){
+        this.gameRepository = gameRepository;
+    }
 }
