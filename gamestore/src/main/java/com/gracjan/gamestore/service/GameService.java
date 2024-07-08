c4b2d9b3ca57e35dfdbd0d4e814a3b8654eec31e
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:20:29 +0200

    Add service GameService

---
+++ gamestore/src/main/java/com/gracjan/gamestore/service/GameService.java
@@ -0,0 +1,4 @@
+package com.gracjan.gamestore.service;
+
+public interface GameService {
+}
---
+++ gamestore/src/main/java/com/gracjan/gamestore/service/GameServiceImpl.java
@@ -0,0 +1,8 @@
+package com.gracjan.gamestore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameServiceImpl implements GameService{
+
+}
