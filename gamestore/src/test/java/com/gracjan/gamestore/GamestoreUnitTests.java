4931d290b694c667783ff868715662a7c5c55ff0
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-08 09:58:54 +0200

    Add BeforeEach and AfterEach to unit tests

--- gamestore/src/test/java/com/gracjan/gamestore/GamestoreUnitTests.java
+++ gamestore/src/test/java/com/gracjan/gamestore/GamestoreUnitTests.java
@@ -6,10 +6,7 @@ import com.gracjan.gamestore.repository.GameJpaRepository;
 import com.gracjan.gamestore.service.GameService;
 import com.gracjan.gamestore.service.GameServiceImpl;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -30,4 +27,16 @@ public class GamestoreUnitTests {
     @InjectMocks
     private GameServiceImpl gameService;
 
+    @BeforeEach
+    public void setData(){
+        System.out.println("Adding 2 games...");
+        games.add(new Game("Minecraft",5,"Sandbox","PC",25.99));
+        games.add(new Game("The Forest",3,"Survival Horror","PC",19.49));
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        System.out.println("Cleaning data...");
+        games.clear();
+    }
 }
