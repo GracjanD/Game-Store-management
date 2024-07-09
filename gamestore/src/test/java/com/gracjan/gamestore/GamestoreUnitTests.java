155a2452ac60cbe231a140592edefe892289515f
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-10 01:15:25 +0200

    Add more unit tests for findAll()

--- gamestore/src/test/java/com/gracjan/gamestore/GamestoreUnitTests.java
+++ gamestore/src/test/java/com/gracjan/gamestore/GamestoreUnitTests.java
@@ -18,8 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,22 +33,90 @@ public class GamestoreUnitTests {
     @InjectMocks
     private GameServiceImpl gameService;
 
+    // findAll()
     @Test
     @Order(1)
-    public void test_findAll(){
-        // set up
+    public void test_findAll_shouldReturnTheSameSize(){
+        // arrange
         when(gameRepository.findAll()).thenReturn(games);
 
-        // execute
+        // act
         List<Game> gameList = gameService.findAll();
 
         // assert
-        assertEquals(games.get(0).getTitle(), gameList.get(0).getTitle(),
-                "Game title should be the same.");
+        // check if the size of the lists are the same
         assertEquals(games.size(), gameList.size(),
                 "Lists should have the same size.");
-        assertNotNull(games.get(0),
-                "Game shouldn't be null");
+    }
+
+    @Test
+    @Order(2)
+    public void test_findAll_shouldBeNotEmpty(){
+        // arrange
+        when(gameRepository.findAll()).thenReturn(games);
+
+        // act
+        List<Game> gameList = gameService.findAll();
+
+        // assert
+        // check if the list is not empty
+        assertFalse(gameList.isEmpty(),
+                "List shouldn't be empty");
+    }
+
+    @Test
+    @Order(3)
+    public void test_findAll_shouldReturnTheSameGames(){
+        // arrange
+        when(gameRepository.findAll()).thenReturn(games);
+
+        // act
+        List<Game> gameList = gameService.findAll();
+
+        // assert
+        // check if the lists are deeply equal
+        assertIterableEquals(games, gameList,
+                "Lists should have the same data");
+
+        // verify if gameRepository was executed only once.
+        verify(gameRepository, times(1)).findAll();
+    }
+
+    @Test
+    @Order(4)
+    public void test_findAll_shouldReturnNotNullGames(){
+        // arrange
+        when(gameRepository.findAll()).thenReturn(games);
+
+        // act
+        List<Game> gameList = gameService.findAll();
+
+        // assert
+        for(int i = 0; i < gameList.size(); i++){
+            // check if the game is not null
+            assertNotNull(games.get(i),
+                    "Game shouldn't be null");
+        }
+
+        // verify if gameRepository was executed only once.
+        verify(gameRepository, times(1)).findAll();
+    }
+
+    @Test
+    @Order(5)
+    public void test_findAll_shouldReturnTheSameTitle(){
+        // arrange
+        when(gameRepository.findAll()).thenReturn(games);
+
+        // act
+        List<Game> gameList = gameService.findAll();
+
+        // assert
+        for(int i = 0; i < gameList.size(); i++){
+            // check if the games have the same title
+            assertEquals(games.get(i).getTitle(), gameList.get(i).getTitle(),
+                    "Game title should be the same.");
+        }
 
         // verify if gameRepository was executed only once.
         verify(gameRepository, times(1)).findAll();
@@ -57,7 +124,7 @@ public class GamestoreUnitTests {
 
     @ParameterizedTest(name="gameId={0}")
     @ValueSource(longs = {0, 1})
-    @Order(2)
+    @Order(6)
     public void test_findById(long value){
         // set up
         when(gameRepository.findById(value)).thenReturn(Optional.ofNullable(games.get((int) value)));
