d55499c09a7fef50df08957fd4201bfe6ed21353
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-26 08:47:10 +0200

    Add userDetailsManager configuration to SecurityConfig

--- gamestore/src/main/java/com/gracjan/gamestore/security/SecurityConfig.java
+++ gamestore/src/main/java/com/gracjan/gamestore/security/SecurityConfig.java
@@ -1,8 +1,27 @@
 package com.gracjan.gamestore.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    // FOR CUSTOM TABLES (accounts, roles INSTEAD OF users, authorities) IN DATABASE
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select username, password, enabled from accounts where username=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select username, role from roles where username=?");
+
+        return jdbcUserDetailsManager;
+    }
 }
