698d6c8094c83e1ff10f1c658d525edd50a60a23
Gracjan Domeracki <129750464+GracjanD@users.noreply.github.com>
2024-07-26 08:55:22 +0200

    Add filterChain configuration to SecurityConfig

--- gamestore/src/main/java/com/gracjan/gamestore/security/SecurityConfig.java
+++ gamestore/src/main/java/com/gracjan/gamestore/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.gracjan.gamestore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -23,5 +27,36 @@ public class SecurityConfig {
                 "select username, role from roles where username=?");
 
         return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        // ADD ROLE PERMISSIONS TO USE ENDPOINTS
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/games").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/games/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/games").hasRole("MODERATOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/games").hasRole("MODERATOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/games").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/games/**").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/dao/games").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/dao/games/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/dao/games/platform/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/dao/games").hasRole("MODERATOR")
+                        .requestMatchers(HttpMethod.PUT, "/dao/games").hasRole("MODERATOR")
+                        .requestMatchers(HttpMethod.DELETE, "/dao/games").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/dao/games/**").hasRole("ADMIN")
+        );
+
+        // USE HTTP BASIC AUTHENTICATION
+        http.httpBasic(Customizer.withDefaults());
+
+        // DISABLE CSRF
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
